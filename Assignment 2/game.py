0ffecd1ab7ae24b4539c901c44357a202580a450
Josh Palmer <joshpalmer123@gmail.com>
2012-03-05 16:12:13 +0000

    fixes: guesses out of bounds, grammar, numerical errors (sleep deprived coding = sloppy coding)

--- Assignment 2/game.py
+++ Assignment 2/game.py
@@ -1,6 +1,7 @@
 # game.py
 # Guessing a random number game thing whatup
 # Josh Palmer 26/2/2k12 etc
+# version 1.1 FIXED A VERY IMPORTANT ERROR IN THE NUMBER OF GUESSES THING!!!!!! also grammar
 
 import random
 
@@ -8,7 +9,13 @@ def Guess():
 	done = 0
 	guesses = []
 	g_count = 0
+	print "I'm thinking of a number between 1 and 100."
 	while done == 0:
+		if g_count == 0:
+			str_guess = " guess."
+		else:
+			str_guess = " guesses."
+		
 		while True:
 			try:
 				x = int(raw_input("Guess the number I'm thinking of: "))
@@ -18,13 +25,19 @@ def Guess():
 
 		if x in guesses:
 			print "You've already guessed this number, try one that you haven't guessed"
+
+		elif ((x < 0) or (x > 100)):
+			print 'Your guess was not between 1 and 100! Try again.'
+		
 		elif x == n:
-			print "Congratulations! You got the number I was thinking of. (It was " + repr(x) + ".) It took you " + repr(g_count) + " guesses."
+			g_count+=1
+			print "Congratulations! You got the number I was thinking of. (It was " + repr(x) + ".) It took you " + repr(g_count) + str_guess
 			done = 1
+		
 		else:
 			guesses.append(x)
 			g_count += 1
-			print "You're on " + repr(g_count) + " guesses."
+			print "You're on " + repr(g_count) + str_guess
 			if x < n:
 				print "Try higher next time."
 			else:
@@ -36,6 +49,7 @@ def main():
 	n = 0
 	while n in [0,25,50,100]:
 		n = random.randint(1,100)
+	print n
 	Guess()
 
 
