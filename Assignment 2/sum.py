8cf8b989f62fa997616730f80557fd0f75273397
Josh Palmer <joshpalmer123@gmail.com>
2012-03-15 20:15:49 +0000

    protected against non-integer input

--- Assignment 2/sum.py
+++ Assignment 2/sum.py
@@ -2,12 +2,14 @@
 # Asks user to add up 2 random numbers
 # Author:     J Palmer
 # Created on: 2012-03-05
-# Version:    1.1
-
-# Random sum thingy
-# does the sum of 2 random numbers
-# Josh Palmer 26/2/2k12 etc
+# Version:    1.2 (protected against non-integer input)
 
 import random
 numbers = tuple([random.randint(1,100) for x in xrange(2)])
-print "You are right!" if int(raw_input("Solution to %d + %d = " % numbers)) == sum(numbers) else "you're wrong :( the correct answer was" + sum(numbers)
+while True:
+		try:
+			n_in = int(raw_input("Solution to %d + %d = " % numbers))
+			break
+		except ValueError:
+			print 'This is not a valid integer! Try again.'
+print "You are right!" if n_in == sum(numbers) else "you're wrong :( The correct answer was " + repr(sum(numbers)) + "."
