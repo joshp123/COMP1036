2dad471e2bfe7339c3a41a816f9a7dca3fa4f594
joshp123 <joshp123@users.noreply.github.com>
2012-03-15 22:07:33 +0000

    made it slightly more elegant - instead of using global variables and incrementing them, the askrand() function returns a value and it works with that

--- Assignment 2/mathstest.py
+++ Assignment 2/mathstest.py
@@ -2,15 +2,12 @@
 # Random sum thing but catches non-ints and repeats 10 times and keeps score
 # Author:     J Palmer
 # Created on: 2012-03-05
-# Version:    1.1 , added displaying correct answer
+# Version:    1.2 made it slightly more elegant - instead of using global variables and incrementing them, the askrand() function returns a value and it works with that 
+
 
 import random
-global c,w
-c = 0
-w = 0
 
-def AskRand():
-	global c,w
+def AskRand(): # Returns 1 if answered correctly, 0 if answered incorrectly
 	numbers = tuple([random.randint(1,100) for x in xrange(2)])
 	while True:
 		try:
@@ -20,16 +17,21 @@ def AskRand():
 			print 'This is not a valid integer! Try again.'
 	if	n_in == sum(numbers):
 		print "You are right!"
-		c +=1
+		return 1
 	else:
 		print "You're wrong :( The correct answer was: " + repr(sum(numbers))
-		w +=1
+		return 0
 
+questions_asked = 0
+correct = 0
+wrong = 0
 for i in xrange(10):
 	print'Question number: ' + str(i+1)
-	AskRand()
+	correct += AskRand()
+	questions_asked +=1
+	wrong = questions_asked - correct
 
-print 'You got ' + repr(c) + ' sums right, and ' + repr(w) + ' sums wrong.'
+print 'You got ' + repr(correct) + ' sums right, and ' + repr(wrong) + ' sums wrong.'
 
 
 
