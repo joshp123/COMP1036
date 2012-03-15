a4960994f9237b5be37afe8ac60fd5a3ac9a0183
joshp123 <joshp123@users.noreply.github.com>
2012-03-15 20:11:18 +0000

    tweaked header

--- Assignment 2/filecheck.py
+++ Assignment 2/filecheck.py
@@ -1,9 +1,10 @@
-# checking if a file exists
-# obviously requires full filename i.e. c:\foo\bar.exe
-#
+# filecheck.py
+# Program to check if a file exists
+# Requires either: full filename i.e. c:\foo\bar.exe, or: filename in the current directory too. (e.g sample.txt)
+# 
 # Author:     J Palmer
 # Created on: 2012-02-26
-# Version:    1.0.1 (added this header lol.)
+# Version:    1.0.2 (tweaked header)
 
 import os
 print "Yes, it exists" if os.path.exists(raw_input("Full filename: ")) else "No, that does not exist"
