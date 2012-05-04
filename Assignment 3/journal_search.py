4f5d917a7d59358aac9a39716e166a45f1c4f9e0
Josh Palmer <joshpalmer123@gmail.com>
2012-05-04 18:29:51 +0100

    suzie smells

--- Assignment 3/journal_search.py
+++ Assignment 3/journal_search.py
@@ -4,9 +4,12 @@
 # Created on: 2012-03-18
 # Version:    1.0.0 [initial barebones non-funtional version]
 
-import os, fileinput, csv
+import os, fileinput, csv, string
 
 def build_dataset(filename):
+    Journals = []
+    Authors = []
+    Title = []
     dataset = []
     with open(filename, 'rbU') as fd:
         reader = csv.reader(fd, delimiter="\t")
@@ -16,14 +19,35 @@ def build_dataset(filename):
                             'year': year,
                             'title': title,
                             'journal': journal})
-
+    # print dataset
     # TODO: error handling
     
     return tuple(dataset)
 
 
+def file_search(data):
+	searchstr = raw_input("Search on author (A = ***) or journal/conference (J = ***), where *** is any string [Q = quit]: ")
+	args = string.split(searchstr)
 
-def FileGetCheck():
+	if args [0] == A:
+		# search authors
+		field = author
+
+	elif args [0] == J:
+		field = journal
+		# search journals
+	elif args [0] == Q:
+		print "Program Exiting"
+		return
+	else:
+		print "Please enter a valid command!"
+	
+	
+
+	return
+
+
+def file_get_check():
 	valid = 0
 	while valid == 0:
 		filename = raw_input("Please specify the file containing your data: ")
@@ -34,9 +58,9 @@ def FileGetCheck():
 	return filename
 
 def main():
-	filename = FileGetCheck()
+	filename = file_get_check()
 	data = build_dataset(filename)
-# 
+
 
 if __name__ == "__main__":
 	main()
